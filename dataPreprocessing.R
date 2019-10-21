47ea7135cf7e96b7138d64afb98c7efabdd20dfd
RohanShetty <shettyrohan641@yahoo.com>
2019-10-21 17:09:40 +0530

    readNprocessIt is a dp GEM!

--- dataPreprocessing.R
+++ dataPreprocessing.R
@@ -1,3 +1,41 @@
 # Football Match Result Predictor
 
 print(' I love Italian League Serie-A ')
+
+#read a file and check how can you do the same using function
+
+setwd('C:\\Users\\Rohan Shetty\\Desktop\\FootballDatasets\\Football-Match-Result-Predictor-\\Datasets')
+
+
+s1718 <- read.csv('SerieA 2017-18.csv')
+head(s1718)
+
+
+colnames(s1718)
+
+
+ii8 <- read.csv('SerieA 2017-05.csv')
+colnames(ii8)
+
+# Remove the irrelevant columns
+
+readNprocessIt <- function(fileName){
+  rf <- read.csv(fileName)
+  data <- as.data.frame(rf)
+  df <- data[c(-1, -23:-65)]
+  head(df)
+}
+readNprocessIt('SerieA 2017-18.csv')
+
+#comment 1(5:08 PM ) this func works fantastic, customize it according to the files like some have 60+ columns other 70+ so in that way
+
+
+list1 <-c(list.files('C:\\Users\\Rohan Shetty\\Desktop\\FootballDatasets\\\\Football-Match-Result-Predictor-\\Datasets'))
+for (i in list1){
+  readFile <- read.csv(i)
+  dataFrame <- as.data.frame(readFile)
+  print(i)
+  print(colnames(dataFrame))
+  }
+
+
