fe9e01f4c7e507e1a4018e63a44918b79c9b3fd8
RohanShetty <shettyrohan641@yahoo.com>
2019-11-06 23:17:24 +0530

    DataViz

--- dataPreprocessing2.R
+++ dataPreprocessing2.R
@@ -38,7 +38,25 @@ head(dpF)
 require(dplyr)
 
 teamwiseAnalysis <- function(homeTeam, awayTeam){
-  dpF %>% filter(HomeTeam == homeTeam & AwayTeam == awayTeam) %>% summarise(FTHG_Avg = mean(FTHG))
+  dpF %>% filter(HomeTeam == homeTeam & AwayTeam == awayTeam) %>% summarise(Home_Team = as.character(HomeTeam), Away_Team = as.character(AwayTeam),
+                                                                            FTHG_Avg = mean(FTHG),
+                                                                            FTAG_Avg = mean(FTAG),
+                                                                            
+                                                                            HTHG_Avg = mean(HTHG),
+                                                                            HTAG_Avg = mean(HTAG),
+                                                                           
+                                                                            HS_Avg = mean(HS),
+                                                                            AS_Avg = mean(AS),
+                                                                            HST_Avg = mean(HST),
+                                                                            AST_Avg = mean(AST),
+                                                                            HF_Avg = mean(HF),
+                                                                            AF_Avg = mean(AF),
+                                                                            HC_Avg = mean(HC), 
+                                                                            AC_Avg = mean(AC), 
+                                                                            HY_Avg = mean(HY), 
+                                                                            AY_Avg = mean(AY),
+                                                                            HR_Avg = mean(HR),
+                                                                            AR_Avg = mean(AR))
 }
 
 for (i in home){
@@ -48,3 +66,5 @@ for (i in home){
 }
 
 teamwiseAnalysis('Juventus','Napoli')
+# this is almost the right way but do some minor changes like changing the data structrure used or some minor ones!
+
