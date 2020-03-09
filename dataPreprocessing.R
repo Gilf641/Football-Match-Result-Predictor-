42cf66319920a98aa6761476d4b9efea5a36d5ae
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-03-09 23:27:41 +0530

    CodeUpdated

--- dataPreprocessing.R
+++ dataPreprocessing.R
@@ -15,61 +15,61 @@ readNprocess <- function(fileName){
   
 }
 
-# # 2007-08
-# seven8 <- readNprocess('SerieA 2007-08.csv')
-# 
-# # 2008-09
-# eight9 <- readNprocess('SerieA 2008-09.csv')
-# 
-# # 2009-10
-# nine10 <- readNprocess('SerieA 2009-10.csv')
-# 
-# # 2010-11
-# ten11 <- readNprocess('SerieA 2010-11.csv')
-# 
-# # 2011-12
-# eleven12 <-readNprocess('SerieA 2011-12.csv')
-# 
-# # 2012-13
-# twelve13 <- readNprocess('SerieA 2012-13.csv')
-# 
-# # 2013-14
-# thirteen14 <-readNprocess('SerieA 2013-14.csv')
-# 
-# # 2014-15
-# fourteen15 <-readNprocess('SerieA 2014-15.csv')
-# 
-# # 2015-16
-# fifteen16 <-readNprocess('SerieA 2015-16.csv')
-# 
-# # 2016-17
-# sixteen17 <-readNprocess('SerieA 2016-17.csv')
-# 
-# # 2017-18
-# seventeen18 <-readNprocess('SerieA 2017-18.csv')
-# 
-# # 2018-19
-# eighteen19 <-readNprocess('SerieA 2018-19.csv')
-# 
-# # 2019-2020
-# nineteen20 <-readNprocess('SerieA 2019-20.csv')
-# 
-# 
+# 2007-08
+seven8 <- readNprocess('SerieA 2007-08.csv')
+
+# 2008-09
+eight9 <- readNprocess('SerieA 2008-09.csv')
+
+# 2009-10
+nine10 <- readNprocess('SerieA 2009-10.csv')
+
+# 2010-11
+ten11 <- readNprocess('SerieA 2010-11.csv')
+
+# 2011-12
+eleven12 <-readNprocess('SerieA 2011-12.csv')
+
+# 2012-13
+twelve13 <- readNprocess('SerieA 2012-13.csv')
+
+# 2013-14
+thirteen14 <-readNprocess('SerieA 2013-14.csv')
+
+# 2014-15
+fourteen15 <-readNprocess('SerieA 2014-15.csv')
+
+# 2015-16
+fifteen16 <-readNprocess('SerieA 2015-16.csv')
+
+# 2016-17
+sixteen17 <-readNprocess('SerieA 2016-17.csv')
+
+# 2017-18
+seventeen18 <-readNprocess('SerieA 2017-18.csv')
+
+# 2018-19
+eighteen19 <-readNprocess('SerieA 2018-19.csv')
+
+# 2019-2020
+nineteen20 <-readNprocess('SerieA 2019-20.csv')
+
+
 
 
 #comment 1(5:08 PM ) this func works fantastic, customize it according to the files like some have 60+ columns other 70+ so in that way
 # 
-# 
-# list1 <-c(list.files('C:\\Users\\Rohan Shetty\\Desktop\\FootballDatasets\\\\Football-Match-Result-Predictor-\\Datasets'))
-# for (i in list1){
-#   readFile <- read.csv(i)
-#   dataFrame <- as.data.frame(readFile)
-#   print(i)
-#   print('\\\\\\\\\\\\')
-#   print(colnames(dataFrame))
-#   print('............')
-#   }
-# 
+
+list1 <-c(list.files('C:\\Users\\Rohan Shetty\\Desktop\\FootballDatasets\\\\Football-Match-Result-Predictor-\\Datasets'))
+for (i in list1){
+  readFile <- read.csv(i)
+  dataFrame <- as.data.frame(readFile)
+  print(i)
+  print('\\\\\\\\\\\\')
+  print(colnames(dataFrame))
+  print('............')
+  }
+
 
 # 
 # 
