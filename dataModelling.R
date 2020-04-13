74cb016bfd9bd791cfa077b4130eb04eceecade3
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-04-13 21:36:08 +0530

    #10, #11

--- dataModelling.R
+++ dataModelling.R
@@ -8,11 +8,7 @@ require(esquisse)
 
 # actually one can create a module and call it as a source 
 avgData <- read.csv('averageStats_SeriaA.csv')
-
-
 summary(avgData)
-view(avgData)
-
 
 plot(avgData$Avg_FTHG~avgData$Avg_HS)
 with(avgData, text(avgData$Avg_FTHG~avgData$Avg_HS, labels = c(HomeTeam, AwayTeam), pos = 3, cex = 0.46))
